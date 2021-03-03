177e70e8b13c47fb5cc535fd2a01271c13fff07c
I.D <i.dazbriones@gmail.com>
2021-03-03 12:46:58 -0300

    Refractor ordenado codigo

--- routes/tiempoactividad.js
+++ routes/tiempoactividad.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const { addtiempoxactividad, updatetiempoxactividad, updatetiempoxactividadfinal} = require('../controllers/tiempoactividad.controller')
+const {
+    addtiempoxactividad,
+    updatetiempoxactividad,
+    updatetiempoxactividadfinal
+} = require('../controllers/tiempoactividad.controller')
 /* GET home page. */
 router.post('/add', addtiempoxactividad);
 router.post('/update/:id', updatetiempoxactividad);
 router.post('/final/:id', updatetiempoxactividadfinal);
-
 
 
 module.exports = router;

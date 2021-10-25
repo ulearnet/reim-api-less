6c67d3a39ddf005cf28b651e3da6a5c5c9432545
pedro fernandes <pedromaturana698@gmail.com>
2021-10-24 23:43:08 -0300

    se agrega metodo get_reacciones_x_img en reaccion_dibujo_controller

--- controllers/Reaccion_dibujo.controller.js
+++ controllers/Reaccion_dibujo.controller.js
@@ -46,10 +46,34 @@ const add_reaccion_dibujo = async (req, res) => {
           }
       }
     );
-
   };
+const get_reacciones_x_img = async (req, res) => {
+    const { id_dibujo_reim } = req.body;
+    console.log(id_dibujo_reim)
+    await pool.query(
+        `SELECT DISTINCT d.id_dibujo_reim, 
+            (SELECT COUNT(idreaccion) FROM reaccionxdibujo WHERE idreaccion = 1 AND iddibujo = d.id_dibujo_reim) Tristeza,
+            (SELECT COUNT(idreaccion) FROM reaccionxdibujo WHERE idreaccion = 2 AND iddibujo = d.id_dibujo_reim) Alegria,
+            (SELECT COUNT(idreaccion) FROM reaccionxdibujo WHERE idreaccion = 3 AND iddibujo = d.id_dibujo_reim) Desagrado,
+            (SELECT COUNT(idreaccion) FROM reaccionxdibujo WHERE idreaccion = 4 AND iddibujo = d.id_dibujo_reim) Temor,
+            (SELECT COUNT(idreaccion) FROM reaccionxdibujo WHERE idreaccion = 5 AND iddibujo = d.id_dibujo_reim) Enojo
+      FROM dibujo_reim d
+      WHERE  d.id_dibujo_reim = ?;`,[id_dibujo_reim],
+        async function (error, results, fields) {console.log(id_dibujo_reim)
+            if (error) throw error;
+            await pool.end()
+            pool.quit()
+            if (results.length > 0) {
+                await res.status(200).json(results);
+            } else {
+                await res.status(404).json(null);
+            }
+        }
+    );
+};
 
 module.exports = {
     add_reaccion_dibujo,
-    get
+    get,
+    get_reacciones_x_img
   };
--- routes/Reaccion_dibujo.js
+++ routes/Reaccion_dibujo.js
@@ -2,10 +2,12 @@ var express = require("express");
 var router = express.Router();
 const {
     add_reaccion_dibujo,
-    get
+    get,
+    get_reacciones_x_img
 } = require("../controllers/Reaccion_dibujo.controller");
 /* GET home page. */
 router.post("/add", add_reaccion_dibujo);
 router.post("/get", get);
+router.post("/get_reacciones", get_reacciones_x_img);
 
 module.exports = router;

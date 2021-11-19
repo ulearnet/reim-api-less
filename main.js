ce5597ca005a3884f2086a6e32c48d988ba7acff
Pedro <pedromaturana698@gmail.com>
2021-11-19 15:45:42 -0400

    arreglo de problema en el main con una de las routes

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ const detalle_usuarioRouter = require("./routes/detalle_usuario");
 const transaccion_reim_router = require("./routes/transaccion_reim");
 const usuario_router = require("./routes/usuario");
 const invitado_router = require("./routes/invitado");
-const imagen_router = require("./routes/imagen");
+const imagen_router = require("./routes/Imagen");
 const evaluacion_router = require("./routes/evaluacion_propuesta");
 
 const main = express();
@@ -66,7 +66,7 @@ main.use("/api/detalle_usuario", detalle_usuarioRouter);
 main.use("/api/transaccion_reim", transaccion_reim_router);
 main.use("/api/usuario", usuario_router);
 main.use("/api/invitado", invitado_router);
-main.use("/api/imagen", imagen_router);
+main.use("/api/Imagen", imagen_router);
 main.use("/api/evaluacion", evaluacion_router);
 
 // catch 404 and forward to error handler

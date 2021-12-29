59821c04cb37f3411cb68a8c2e973df0f064254e
nicolaslefian <n.lefingutirrez@uandresbello.edu>
2021-12-28 23:06:07 -0300

    verificacion de asigna_reim en login

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -2,14 +2,17 @@ const { pool } = require("../dbcnx");
 const md5 = require("md5");
 
 const login = async (req, res) => {
-  const { loginame, password } = req.body;
+  const { loginame, password} = req.body;
+  const reim_id = req.params.reim_id;
 
   await pool.query(
-    `select id, concat(nombres, apellido_paterno, apellido_materno) as nombre
-     from usuario
-     where username = ?
-     and password = ?  
-     `,
+    `select id, concat(nombres, apellido_paterno, apellido_materno) as nombre from usuario u 
+     where username = ? 
+     and password = ? 
+     and id in (select usuario_id from pertenece where usuario_id = u.id 
+                and nivel_id in (select nivel_id from asigna_reim where reim_id = ${reim_id})
+                and letra_id in (select letra_id from asigna_reim where reim_id = ${reim_id} )
+                and colegio_id in (select colegio_id from asigna_reim where reim_id = ${reim_id}))`,
     [loginame, password],
     async function (error, results, fields) {
         if (error) throw error;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ var express = require("express");
 var router = express.Router();
 const { login } = require("../controllers/login.controller");
 /* GET home page. */
-router.post("/login", login);
+router.post("/login/:reim_id", login);
 
 module.exports = router;

0b893a5beaed3fb64b3ed9aa41a60df44a0f0df5
nicolaslefian <n.lefingutirrez@uandresbello.edu>
2021-12-31 04:13:08 -0300

    Creacion de consulta en usuario.controller para obtener nickname por usuario

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -28,6 +28,34 @@ const getNombre = async (req, res) => {
 
 };
 
+const getNickname = async (req, res) => {
+  const { id } = req.body;
+
+  await pool.query(
+    `select nick_name
+    from usuario 
+    where id = ?`,
+    [id],
+    async function (error, results, fields) {
+        if (error) throw error;
+        await pool.end()
+        pool.quit()
+        if (results.length > 0) {
+
+            const nombre = results[0];
+            await res.status(200).json(nombre);
+
+        } else {
+
+            await res.status(404).json(null);
+
+        }
+    }
+  );
+
+};
+
 module.exports = {
-  getNombre
+  getNombre,
+  getNickname
 };
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var router = express.Router();
 const {
-    getNombre
+    getNombre,
+    getNickname
 } = require("../controllers/usuario.controller");
 /* GET home page. */
 router.post("/getNames", getNombre);
+router.post("/getNickname", getNickname);
 
 module.exports = router;
